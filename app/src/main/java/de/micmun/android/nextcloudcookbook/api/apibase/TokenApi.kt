b1e467fd25200be05f5409d8b34236955011ea81
timbze <tim.bze@gmail.com>
2021-08-21 11:42:18 -0600

    Add User-Agent to app-password request

--- app/src/main/java/de/micmun/android/nextcloudcookbook/api/apibase/TokenApi.kt
+++ app/src/main/java/de/micmun/android/nextcloudcookbook/api/apibase/TokenApi.kt
@@ -4,7 +4,7 @@ import de.micmun.android.nextcloudcookbook.api.apimodel.TokenResponse
 import retrofit2.http.*
 
 interface TokenApi {
-    @Headers("OCS-APIRequest: true")
+    @Headers("OCS-APIRequest: true", "User-Agent: Nextcloud Cookbook Android")
     @GET("ocs/v2.php/core/getapppassword?format=json")
     suspend fun getToken(@Header("Authorization") basicAuthCredentials: String): TokenResponse?
 }
