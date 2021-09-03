38fb71fb2ff1cb879b5beedcfadc29b71c9e5c29
timbze <tim.bze@gmail.com>
2021-09-03 14:59:15 -0600

    Add MyCookBox recipe converter

---
+++ app/src/main/java/de/micmun/android/nextcloudcookbook/util/xml/MyCookboxConverter.kt
@@ -0,0 +1,7 @@
+package de.micmun.android.nextcloudcookbook.util.xml
+
+/**
+ * Converts recipe backups from/to "my CookBox" app: https://play.google.com/store/apps/details?id=com.mycookbox.android
+ */
+class MyCookBoxConverter {
+}
